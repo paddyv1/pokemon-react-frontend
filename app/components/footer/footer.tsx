5ba7f41cbd5c78d77cba92a22aa18c1ad493423d
Patrick Talty-Kerr <patricktaltykerr@outlook.com>
2026-03-19 00:07:08 +0000

    finished simple footer implementation

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,7 +1,55 @@
 export default function Footer() {
   return (
     <>
-      <footer className="bg-purple-800 h-45">This is the footer</footer>
+      <footer className="bg-slate-400 rounded-base shadow-xs ">
+        <div className="w-full max-w-7xl mx-auto p-4 md:py-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <a
+              href="https://github.com/paddyv1"
+              className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+            >
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/960px-Octicons-mark-github.svg.png?_=20180806170715"
+                className="h-7"
+                alt="Flowbite Logo"
+              />
+              <span className="text-heading self-center text-2xl font-semibold whitespace-nowrap">
+                paddyv1
+              </span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-body sm:mb-0">
+              <li>
+                <a href="" className="hover:underline me-4 md:me-6">
+                  Im not
+                </a>
+              </li>
+              <li>
+                <a href="" className="hover:underline me-4 md:me-6">
+                  Clouted Enough
+                </a>
+              </li>
+              <li>
+                <a href="" className="hover:underline me-4 md:me-6">
+                  To Have These
+                </a>
+              </li>
+              <li>
+                <a href="" className="hover:underline">
+                  Yet....
+                </a>
+              </li>
+            </ul>
+          </div>
+          <hr className="my-6 border-default sm:mx-auto lg:my-8" />
+          <span className="block text-sm text-body sm:text-center">
+            © 2026{" "}
+            <a href="https://github.com/paddyv1" className="hover:underline">
+              whoneedsthis™
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
+      </footer>
     </>
   );
 }
--- app/routes/welcome/welcome.tsx
+++ app/routes/welcome/welcome.tsx
@@ -3,23 +3,26 @@ import khangaskhan from "./khangkaskhan.jpg";
 import Carousel from "./carousel";
 export function Welcome() {
   return (
-    <div className="grid grid-cols-3 gap-4 text-black pt-15">
-      <div className="col-span-2 bg-green-300 h-150 font-extrabold text-9xl">
+    <div className="grid grid-cols-3 gap-4 text-black pt-15 pb-10">
+      <div className=" col-span-2 pl-22 bg-green-300 h-150 font-extrabold text-9xl rounded-3xl">
         Welcome to VGC Central
       </div>
-      <div className="bg-green-600 h-0 ">
-        <img className="object-cover" src={incineroar} />
+      <div className="bg-green-600 h-0">
+        <img className="object-cover rounded-2xl" src={incineroar} />
       </div>
       <div className="col-span-3 overflow-y-visible">
         <Carousel />
       </div>
-      <div className=" bg-red-50 h-170 text-8xl">
+      <div className=" bg-red-50 h-170 text-8xl rounded-4xl">
         Master your type match ups with our mini games
       </div>
-      <div className="flex justify-center-safe  bg-amber-600 h-100">
-        <img className="object-fill h-140 w-full" src={khangaskhan} />
+      <div className="flex justify-center-safe  h-100 rounded-2xl">
+        <img
+          className="object-fill h-140 w-full rounded-4xl"
+          src={khangaskhan}
+        />
       </div>
-      <div className=" bg-blue-200 h-160 text-7xl text-right">
+      <div className=" bg-blue-200 h-160 text-7xl text-right rounded-2xl">
         Collaborate Live with Friends to create teams which can be exported into
         a Pokemon Showdown
       </div>
