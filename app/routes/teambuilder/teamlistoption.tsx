9b6f403a568f5ffd52898974a3f6f4e94e42036c
Patrick Talty-Kerr <patricktaltykerr@outlook.com>
2026-03-19 22:21:10 +0000

    added ts pokeapi client

--- app/routes/teambuilder/teamlistoption.tsx
+++ app/routes/teambuilder/teamlistoption.tsx
@@ -1,10 +1,26 @@
+import { PokemonClient } from "pokenode-ts"; // import the PokemonClient
+import { useState } from "react";
+
 export default function ListChoice() {
+  const [pokemonName, UpdatePokemon] = useState<string | null>(null);
+
+  const api = new PokemonClient(); // create a PokemonClient
+
+  const res = api
+    .getPokemonById(1)
+    .then((res) => {
+      UpdatePokemon(res.sprites.front_default);
+    })
+    .catch((error) => console.error(error));
+
   return (
     <>
       <section className="border h-45 rounded-3xl">
         <div className="flex justify-center">Team Name</div>
-        <div className="grid grid-cols-3 grid-rows-2 gap-10">
-          <div>Sprite 1</div>
+        <div className="grid grid-cols-3 grid-rows-2 gap-5">
+          <div className="">
+            <img src={pokemonName} />
+          </div>
           <div>Sprite 2</div>
           <div>Sprite 3</div>
           <div>Sprite 4</div>
