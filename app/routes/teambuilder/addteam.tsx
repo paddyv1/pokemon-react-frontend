716c8bfc3201c7b2cba1636b4d4d4fa838613d54
Patrick Talty-Kerr <patricktaltykerr@outlook.com>
2026-03-19 21:26:55 +0000

    easie in out

--- app/routes/teambuilder/addteam.tsx
+++ app/routes/teambuilder/addteam.tsx
@@ -2,7 +2,7 @@ import plus from "../../resources/plus.svg";
 
 export default function AddTeam() {
   return (
-    <div className="h-15 inset-0 backdrop-blur-[1px] flex justify-center px-5 rounded-3xl outline-1 hover:-translate-y-1 hover:transition-all">
+    <div className="h-15 inset-0 backdrop-blur-[1px] flex justify-center px-5 rounded-3xl outline-1 ease-in-out transition-all hover:-translate-y-1 hover:transition-all">
       <button>
         <img src={plus} className="h-10"></img>
       </button>
